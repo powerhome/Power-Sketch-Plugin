281d8476cd7eb232e79c2b60be5e90a3f3bb7d8d
christina tai <christina.tai@powerhrg.com>
2020-11-19 12:55:42 -0500

    test

---
+++ constants/departments.js
@@ -0,0 +1,24 @@
+const DEPT = [
+    "Accounting",
+    "BT Systems",
+    "Business Affairs",
+    "Business Technology",
+    "Communications",
+    "Contact Center",
+    "Corporate Events",
+    "Customer Development",
+    "Executive",
+    "Facilities",
+    "Financing",
+    "Human Relations",
+    "Legal",
+    "Marketing Operations",
+    "Military Affairs",
+    "Operations – Corp",
+    "Operations – Field",
+    "Sales",
+    "Talent Acquisition",
+    "Vendors"
+]
+
+module.exports = DEPT;
---
+++ constants/territories.js
@@ -0,0 +1,21 @@
+const TERR = [
+    "Philadelphia",
+    "New Jersey",
+    "Maryland",
+    "Connecticut",
+    "Long Island",
+    "Boston",
+    "Atlanta",
+    "Chicago",
+    "Detroit",
+    "Houston",
+    "Dallas",
+    "Denver",
+    "Tampa",
+    "Austin",
+    "Charlotte",
+    "Nashville",
+    "Phoenix"
+]
+
+module.exports = TERR;
