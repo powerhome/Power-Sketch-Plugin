98ea3f934e7b185894d714241250942132a828dd
christina tai <christina.tai@powerhrg.com>
2021-01-07 12:00:21 -0500

    Added months data set

--- src/phrg.js
+++ src/phrg.js
@@ -1,17 +1,18 @@
 const sketch = require('sketch')
 const { DataSupplier } = sketch
-const util = require('util')
 
 //constants
 const DEPT = require("../constants/departments");
 const TERR = require("../constants/territories");
 const TERR_ABBR = require("../constants/territories_abbr");
+const MONTHS = require("../constants/months");
 
 //startup
 export function onStartup () {
   DataSupplier.registerDataSupplier('public.text', 'Departments', 'SupplyDepartments');
   DataSupplier.registerDataSupplier('public.text', 'Territories', 'SupplyTerritories');
   DataSupplier.registerDataSupplier('public.text', 'Territories (abbr)', 'SupplyTerritoriesAbbr');
+  DataSupplier.registerDataSupplier('public.text', 'Months', 'SupplyMonths');
 }
 
 //shutdown
@@ -54,6 +55,19 @@ export function onSupplyTerritoriesAbbr(context) {
   while (dataIndex < dataCount) {
       const terr_abbr = sample(TERR_ABBR);
       DataSupplier.supplyDataAtIndex(dataKey, terr_abbr, dataIndex);
+      dataIndex++;
+  }
+}
+
+//months
+export function onSupplyMonths(context) {
+  var dataKey = context.data.key;
+  var dataCount = context.data.requestedCount;
+
+  var dataIndex = 0;
+  while (dataIndex < dataCount) {
+      const months = sample(MONTHS);
+      DataSupplier.supplyDataAtIndex(dataKey, months, dataIndex);
       dataIndex++;
   }
 }
