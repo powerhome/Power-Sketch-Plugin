892dfae964ee3a866df27aa462fe46ff43a609f5
christina tai <christina.tai@powerhrg.com>
2021-01-12 10:32:10 -0500

    Added month abbr data set

---
+++ constants/months_abbr.js
@@ -0,0 +1,16 @@
+const MONTHS_ABBR = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec"
+]
+
+module.exports = MONTHS_ABBR;
